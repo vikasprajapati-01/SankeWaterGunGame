e666fec7ea38aa3cec292dd33147a7b8514ca191
vikasprajapati-01 <vikasprajapati9980@gmail.com>
2023-08-17 12:13:16 +0530

    adding some developments

--- SnakeWaterGunGame.java
+++ SnakeWaterGunGame.java
@@ -28,13 +28,20 @@ public class SnakeWaterGunGame
         Random rn = new Random();
         int rnum =rn.nextInt(100)+1;
         char ch, user, comp;
-        System.out.println("Enter 'S' for Snake, 'W' for Water or 'G' for Gun ");
+        // Adding loop until player wishes to exit 
+        while(true){
+        // Taking input from user 
+        System.out.println("Enter 'S' for Snake, 'W' for Water or 'G' for Gun and Q for exit");
         ch =sc.next().charAt(0);
         user = Character.toUpperCase(ch);
+
         // ------check whether input is valid or not---------
+        if(user !='Q')
+        {
         if(user != 'S' && user != 'W' && user != 'G')
         {
             System.out.println("Invalid input, Please enter 'W','G', or 'S'.");
+            continue;
         }
         // ------- Assiging char value to computer------
         if(rnum<=33)
@@ -49,6 +56,7 @@ public class SnakeWaterGunGame
         {
             comp = 'G';
         }
+        // sending the values to the functions
         int result =check(user,comp);
         // ---------Finding and displaying the result-------
         if(result==1)
@@ -59,11 +67,18 @@ public class SnakeWaterGunGame
         {
             System.out.println("Nice try !!! But the match is tie");
         }
-        else
+        else if(result==0)
         {
             System.out.println("Oops !!! You have lost, Better luck next time.");
         }
-        System.out.println("Because you chose " + user + " and the coputer chose " + comp);
+        System.out.println("Because you chose " + user + " and the computer chose " + comp);
+    }
+    else if(user=='Q')
+    {
+        System.out.println("Thank you for playing the game!");
+        break;
+    }
+    }
         sc.close();
     }
 }
